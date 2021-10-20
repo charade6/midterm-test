d8ba13ae74f074844318a38c87470115a423bcf7
charade6 <charade6@naver.com>
2021-10-20 11:47:12 +0900

    Third commit
    정상동작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react'
 
-function App() {
-  return (
-    <div> 
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    fruits: null
+  }
+  apple = () => {
+    this.setState({fruits: "사과."})
+  }
+  orange = () => {
+    this.setState({fruits: "오렌지."})
+  }
+  banana = () => {
+    this.setState({fruits: "바나나."})
+  }
+  render() {
+    return (
+      <div>
+        <h1>어떤 과일을 좋아하나요? {this.state.fruits}</h1>
+        <button onClick={this.apple}>사과</button>
+        <button onClick={this.orange}>오렌지</button>
+        <button onClick={this.banana}>바나나</button>
+      </div>
+    )
+  }
 }
 
 export default App
